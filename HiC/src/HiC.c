624f538b66631faed41cd9f8086427ed15c9df82
Keith Collins <kcollins2004@gmail.com>
2016-01-23 07:45:58 -0500

    Changed greeting message.

--- HiC/src/HiC.c
+++ HiC/src/HiC.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : HiC.c
- Author      : 
+ Author      :
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hi World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
